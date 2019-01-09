d272db17b904d80c4a0f749d3c792483ae18b4c5
ranton <ranton@Dads-Retina-MacBookPro.local>
2019-01-09 04:01:51 -0500

    Started over

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TriviaGame
 JavaSscript Assignment 2
+This assignment started out with good intentions:
+To create a fun, interactive, game that was based on the music by the reggae music legend, Robert Nesta 'Bob' Marley. 
+I had the good fortune of meeting him in his Kingston, Jamaica home in 1976 while there as an exchange student and thought this would be a fitting tribute.
+**However, rather than enter the murky waters of music copyright laws, I chose to avoid those potential complications. 
+As a result, we now have a relatively straightforward test that is based on human anatomy.  After all, we all have a body and how well do we really know it?  
+Hopefully we can learn something about the complex, but wonderful you.  
+
+I started with chosing male or female anatomy to create two different sets of questions--
---
+++ assets/javascript/app.js
@@ -0,0 +1,35 @@
+// ------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------
+// Trivia game JavaScript #2
+const quizContainer = document.getElementById("quiz");
+const resultsContainer = document.getElementById("results");
+const submitButton = document.getElementById('submit');
+let tToken = .5;
+
+$(document).ready(function() {
+    $("#timer").click(function() {
+        $("#timer").startTimer(duration, display); {
+            tToken = .5
+            var timer = duration, minutes, seconds;
+                setInterval(function () {
+                    minutes = parseInt(timer / 60, 10)
+                    seconds = parseInt(timer % 60, 10);
+
+                    minutes = minutes < 10 ? "0" + minutes : minutes;
+                    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+                    display.textContent = minutes + ":" + seconds;
+
+                if (--timer < 0) {
+                    timer = duration;
+                }
+        }, 1000);
+    }   
+
+    window.onload = function () {
+        var zeroMinutes = 60 * tToken,
+        display = document.querySelector('#time');
+        startTimer(zeroMinutes, display);
+    };
+})
+})
