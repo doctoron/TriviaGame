c10bbd423615f1b5b7d23e32e8be598ad57a0f0d
ranton <ranton@Dads-Retina-MacBookPro.local>
2019-01-13 18:06:25 -0500

    Changes to timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,75 @@
-// ------------------------------------------------------------------------------------
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// 
 // ------------------------------------------------------------------------------------
 // Trivia game JavaScript
-const quizContainer = document.getElementById("quiz");
-const resultsContainer = document.getElementById("results");
-const submitButton = document.getElementById('submit');
-let tToken = .5;
-
-$(document).ready(function() {
-    $("#timer").click(function() {
-        $("#timer").startTimer(duration, display); {
-            tToken = .5
-            var timer = duration, minutes, seconds;
-                setInterval(function () {
-                    minutes = parseInt(timer / 60, 10)
-                    seconds = parseInt(timer % 60, 10);
-
-                    minutes = minutes < 10 ? "0" + minutes : minutes;
-                    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-                    display.textContent = minutes + ":" + seconds;
-
-                if (--timer < 0) {
-                    timer = duration;
-                }
-        }, 1000);
-    }   
-
-    window.onload = function () {
-        var zeroMinutes = 60 * tToken,
-        display = document.querySelector('#time');
-        startTimer(zeroMinutes, display);
+// ------------------------------------------------------------------------------------
+// Thanks to influences by Stopwatch activity, Michael Pascuczzi &
+// Tamekia Schatzmann for helping with the timer logic code
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+$(document).ready(function(){
+// Initialize variables:
+let timerDisplay = document.getElementById('timer-display')
+let tRemaining = document.getElementById('time-remaining');
+isCounting = "false";
+tToken = 30;
+// 
+// ------------------------------------------------------------------------------------
+// Start the clock and display question
+// ------------------------------------------------------------------------------------
+    $("#start-button").click(function() {
+        console.log("The button was clicked")
+        initFunction = () => {
+            tRemaining.innerHTML;
+            tDisplay.innerHTML;
+            stopTimer();
+            startTimer(tToken,timerDisplay);
+            console.log(tToken);
     };
+    
+let startTimer = (tToken, display) => {
+    if (!isCounting) {
+        isCounting = true;
+        var timer = tToken, minutes, seconds;
+        intervalId =  setInterval(function () {
+
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = tToken;
+        }
+    }, 1000);
+}};
+
+// ClearInterval stops the count  and set the clock to not be counting.
+let stopTimer = () => {
+    clearInterval(intervalId);
+    isCounting = false;
+};
+ 
 })
 })
+
+function timeConverter(t) {
+  
+    var minutes = Math.floor(t/60);
+    var seconds = t - (minutes * 60);
+  
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+  
+    if (minutes === 0) {
+      minutes = "00";
+    }
+    else if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+  
+    return minutes + ":" + seconds;
+  }
