8dbe490ef1112c39694b6f8a4eb5afac737d51fd
ranton <ranton@Dads-Retina-MacBookPro.local>
2019-01-14 00:02:59 -0500

    Changes to timer and progess comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,49 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-// 
-// ------------------------------------------------------------------------------------
 // Trivia game JavaScript
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // ------------------------------------------------------------------------------------
 // Thanks to influences by Stopwatch activity, Michael Pascuczzi &
 // Tamekia Schatzmann for helping with the timer logic code
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// ------------------------------------------------------------------------------------
+/*
+Here’s a good description of modulo/modulus which 
+is the “%” symbol: 
+https://en.m.wikipedia.org/wiki/Modulo_operation   
+What is happening is the timer takes the total time 
+(let’s say 100 seconds) and it assigns minutes by 
+dividing 100 by 60. It finds seconds similarly using 
+the modulus. 60 goes into 100 exactly 1 time and leaves 
+a remainder of 40. 40 would be the seconds. 
+Something along those lines. (edited)
+  
+Plan: 
+1) Switch() Case: Default: logic 
+2) My Date translation 
+3) Construct Ajax calls to either YouTube, using jQuery  
+*/    
+
 $(document).ready(function(){
 // Initialize variables:
-let timerDisplay = document.getElementById('timer-display')
-let tRemaining = document.getElementById('time-remaining');
+let tDisplay = document.getElementById("timer-display");
+let tRemaining = document.getElementById("time-remaining");
 isCounting = "false";
-tToken = 30;
-// 
-// ------------------------------------------------------------------------------------
+tToken = 6;
 // Start the clock and display question
 // ------------------------------------------------------------------------------------
     $("#start-button").click(function() {
         console.log("The button was clicked")
         initFunction = () => {
-            tRemaining.innerHTML;
-            tDisplay.innerHTML;
+            tRemaining.innerHTML =`Time remaining: ${display} minutes!`;
             stopTimer();
-            startTimer(tToken,timerDisplay);
+            startTimer(tToken,tDisplay);
             console.log(tToken);
     };
     
-let startTimer = (tToken, display) => {
+let startTimer = (duration, display) => {
     if (!isCounting) {
         isCounting = true;
-        var timer = tToken, minutes, seconds;
+        var timer = duration, minutes, seconds;
         intervalId =  setInterval(function () {
-
         minutes = parseInt(timer / 60, 10)
         seconds = parseInt(timer % 60, 10);
 
@@ -41,7 +53,7 @@ let startTimer = (tToken, display) => {
         display.textContent = minutes + ":" + seconds;
 
         if (--timer < 0) {
-            timer = tToken;
+            timer = duration;
         }
     }, 1000);
 }};
@@ -54,22 +66,3 @@ let stopTimer = () => {
  
 })
 })
-
-function timeConverter(t) {
-  
-    var minutes = Math.floor(t/60);
-    var seconds = t - (minutes * 60);
-  
-    if (seconds < 10) {
-      seconds = "0" + seconds;
-    }
-  
-    if (minutes === 0) {
-      minutes = "00";
-    }
-    else if (minutes < 10) {
-      minutes = "0" + minutes;
-    }
-  
-    return minutes + ":" + seconds;
-  }
