c095bb6dcb9a514e30412c9d3d197ea7d0741970
doctoron <ron.antonio@me.com>
2019-02-06 21:39:19 -0500

    CSS changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -74,10 +74,7 @@ var game = {
     correct: 0,
     incorrect: 0,
 
-<<<<<<< HEAD
     // Interval countdown function here
-=======
->>>>>>> 5eda9de186be2930d0641cdad1d0fb20c3b2615a
     countdown: function () {
         this.counter--;
         $("#counter-number").text(this.counter);
@@ -85,7 +82,6 @@ var game = {
             console.log("TIME'S UP");
             this.timeUp();
         }
-<<<<<<< HEAD
     },
 
     loadQuestion: function () {
@@ -191,112 +187,6 @@ var game = {
             setTimeout(this.nextQuestion.bind(this), 3 * 1000);
         }
     },
-=======
-    },
-
-    loadQuestion: function () {
-        // Binding the timer interval countdown function to the current question
-        timer = setInterval(this.countdown.bind(this), 1000);
-
-        card.html("<h2>" + questions[this.currentQuestion].question + "</h2>");
-
-        // Dynamically generate question one at a time
-        for (var i = 0; i < questions[this.currentQuestion].answers.length; i++) {
-            // Cursor position becomes clickable event
-            card.append("<button class='answer-button' id='button' data-name='" + questions[this.currentQuestion].answers[i]
-                + "'>" + questions[this.currentQuestion].answers[i] + "</button>");
-        }
-    },
-
-    nextQuestion: function () {
-        this.counter = window.parseInt(countStartNumber);
-        $("#counter-number").text(this.counter);
-        this.currentQuestion++;
-        this.loadQuestion.bind(this)();
-    },
-
-    timeUp: function () {
-        console.log(window.timer);
-        clearInterval(window.timer);
-
-        $("#counter-number").text(this.counter);
-
-
-        card.html("<h2>Time's Up!</h2>");
-        card.append("<h3>The Correct Answer was: " + questions[this.currentQuestion].correctAnswer);
-        card.append("<img src='" + questions[this.currentQuestion].wrong + "' />");
-
-        if (this.currentQuestion === questions.length) {
-            setTimeout(this.results, 3 * 1000);
-
-        }
-        else {
-            setTimeout(this.nextQuestion, 3 * 1000);
-            card.append("<br><button id='start-over'>Start Over?</button>");
-
-        }
-    },
-
-    results: function () {
-
-        clearInterval(window.timer);
-
-        card.html("<h2>All done! Here's how you did:</h2>");
-
-        $("#counter-number").text(this.counter);
-
-        card.append("<h3>Correct Answers: " + this.correct + "</h3>");
-        card.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
-        card.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
-        card.append("<br><button id='start-over'>Start Over?</button>");
-    },
-
-    clicked: function (e) {
-        clearInterval(window.timer);
-        if ($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer) {
-            this.answeredCorrectly();
-        }
-        else {
-            this.answeredIncorrectly();
-        }
-    },
-
-    answeredIncorrectly: function () {
-        // add to wrong answer counter
-        this.incorrect++;
-
-        clearInterval(window.timer);
-
-        card.html("<h2>Sorry, That's Incorrect!</h2>");
-        card.append("<h3>The Correct Answer was: " + questions[this.currentQuestion].correctAnswer + "</h3>");
-        card.append("<img src='" + questions[this.currentQuestion].wrong + "' />");
-
-        if (this.currentQuestion === questions.length - 1) {
-            setTimeout(this.results.bind(this), 3 * 1000);
-        }
-        else {
-            setTimeout(this.nextQuestion.bind(this), 3 * 1000);
-        }
-    },
-
-    answeredCorrectly: function () {
-
-        clearInterval(window.timer);
-
-        this.correct++;
-
-        card.html("<h2>Correct!</h2>");
-        card.append("<img src='" + questions[this.currentQuestion].reward + "' />");
-
-
-        if (this.currentQuestion === questions.length - 1) {
-            setTimeout(this.results.bind(this), 3 * 1000);
-        }
-        else {
-            setTimeout(this.nextQuestion.bind(this), 3 * 1000);
-        }
-    },
->>>>>>> 5eda9de186be2930d0641cdad1d0fb20c3b2615a
     reset: function () {
         this.currentQuestion = 0;
         this.counter = countStartNumber;
